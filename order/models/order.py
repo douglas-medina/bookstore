1f54fa37773f7704d3823836f69dc52260f94faa
douglas-medina <medina.douglas.dmv@gmail.com>
2023-11-27 23:25:03 -0300

    add conteudo need fix

--- order/models/order.py
+++ order/models/order.py
@@ -1,8 +1,10 @@
-from django.db import models
 from django.contrib.auth.models import User
+from django.db import models
 
 from product.models import Product
 
+
 class Order(models.Model):
+    id = models.AutoField(primary_key=True)
     product = models.ManyToManyField(Product, blank=False)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
